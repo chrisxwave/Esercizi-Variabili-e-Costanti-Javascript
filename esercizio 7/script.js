2b84cb2bd81895cc6566120df0783ba07de4a77c
chrisxwave <chrisilmer914@gmail.com>
2026-06-24 20:49:46 +0200

    commit 7 esrecizio

---
+++ esercizio 7/script.js
@@ -0,0 +1,9 @@
+//Esercizio 7 (opzionale)
+//Dichiarare una variabile chiamata "persona" e assegnarle un oggetto con le seguenti proprietà: "nome" (con valore "Mario") e "età" (con valore 30). Stampare l'oggetto sulla console.
+
+let persona ={
+nome: "Mario",
+eta: 30,
+};
+
+console.log(persona);
