9799a7bbc274fea2da46335355d52cb1fd8cc30c
chrisxwave <chrisilmer914@gmail.com>
2026-06-24 20:17:27 +0200

    commit 3 esercizio

---
+++ esercizio 3/script.js
@@ -0,0 +1,7 @@
+//Esercizio 3 Dichiarare una variabile chiamata "nome" e assegnarle il valore "Marco". Successivamente, modificare il valore della variabile in "Luca". Stampare il nuovo valore sulla console.
+
+let nome = "Marco";
+ 
+nome = "Luca";
+
+console.log(nome);
